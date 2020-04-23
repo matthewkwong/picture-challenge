e41c40d03180cdf8903388b5aa0a23972d7a7008
Matthew Kwong <matthchewkwong@gmail.com>
2020-04-23 11:27:09 -0700

    finished project

--- script1.js
+++ script1.js
@@ -1,26 +1,46 @@
-var video = document.querySelector("video");
+const player = document.getElementById('player');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const captureButton = document.getElementById('capture');
 
-if(navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({
-        video:true
-    })
-    .then(function(stream){
-        video.srcObject = stream;
-    })
-    .catch(function(error){
-        console.log("Error");
-    })
+const constraints = {
+    video: true,
+};
+
+// captureButton.addEventListener('click', () => {
+//     // Draw the video frame to the canvas.
+//     context.drawImage(player, 0, 0, canvas.width, canvas.height);
+// });
+
+
+// function that takes pictures and prints to new canvas
+
+picture = () => {
+    // Make new canvas
+    var x = document.createElement("CANVAS");
+    var ctx = x.getContext("2d");
+    // Draws the video frame to the canvas
+    ctx.drawImage(player, 0, 0, canvas.width, canvas.height);
+    document.body.appendChild(x);
+
+    console.log("new canvas");
+    // context.drawImage(player, 0, 0, canvas.width, canvas.height);
+    // console.log("new image");
 }
 
 
 
 
+// Attach the video stream to the video element and autoplay.
+navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+    player.srcObject = stream;
+});
 
-// Timer function to take pictures every 1 second
-
-function timer(){
-    // eventually replace the photo taking function;
-    setInterval(() => console.log('photo taken'), 1000);   
+// 1 second timer to take pics
+timer = () => {
+    console.log("new picture taken");
+    setInterval(() => picture(), 1000);   
 }
 
 timer();
