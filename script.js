ce936f6fc90c1abc7c34ae32450bbfd8a7099874
Matthew Kwong <matthchewkwong@gmail.com>
2020-04-28 01:18:55 -0700

    application does not start unless user grants permission to use camera. added padding to each image.

--- script.js
+++ script.js
@@ -1,107 +1,56 @@
-(function() {
-  // The width and height of the captured photo. We will set the
-  // width to the value defined here, but the height will be
-  // calculated based on the aspect ratio of the input stream.
+const player = document.getElementById('player');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const captureButton = document.getElementById('capture');
 
-  var width = 320;    // We will scale the photo width to this
-  var height = 0;     // This will be computed based on the input stream
+// Default camera permission is on
+const constraints = {
+    video: true,
+};
 
-  // |streaming| indicates whether or not we're currently streaming
-  // video from the camera. Obviously, we start at false.
 
-  var streaming = false;
+// Checks if user lets camera permissions on
+var successCallback = function() {
+  timer();
+};
 
-  // The various HTML elements we need to configure or control. These
-  // will be set by the startup() function.
-
-  var video = null;
-  var canvas = null;
-  var photo = null;
-  var startButton = null;
-
-  function startup() {
-    video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
-    photo = document.getElementById('photo');
-    startButton = document.getElementById('startButton');
-
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-    .then(function(stream) {
-      video.srcObject = stream;
-      video.play();
-    })
-    .catch(function(err) {
-      console.log("An error occurred: " + err);
-    });
-
-    video.addEventListener('canplay', function(ev){
-      if (!streaming) {
-        height = video.videoHeight / (video.videoWidth/width);
-      
-        // Firefox currently has a bug where the height can't be read from
-        // the video, so we will make assumptions if this happens.
-      
-        if (isNaN(height)) {
-          height = width / (4/3);
-        }
-      
-        video.setAttribute('width', width);
-        video.setAttribute('height', height);
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
-        streaming = true;
-      }
-    }, false);
-
-    startButton.addEventListener('click', function(ev){
-      takepicture();
-      ev.preventDefault();
-    }, false);
-    
-    clearphoto();
+var errorCallback = function() {
+  if (error.name == 'NotAllowedError') {
+    console.log("user denied access to camera");
   }
+};
 
-  // Fill the photo with an indication that none has been
-  // captured.
 
-  function clearphoto() {
-    var context = canvas.getContext('2d');
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+// Attach the video stream to the video element and autoplay.
+navigator.mediaDevices.getUserMedia(constraints)
+    .then((stream) => {
+    player.srcObject = stream;
 
-    var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
-  }
+    // Checks if users approve video camera 
+    successCallback();
+    errorCallback();
+});
+
+// Picture taking function
+picture = () => {
+  // var containerDiv = document.createElement("div");
+  // containerDiv.appendChild(newCanvas);
+  // Make new canvas
+  console.log("new canvas");
+  var newCanvas = document.createElement("CANVAS");
+  var ctx = newCanvas.getContext("2d");
+
+  // Draws the video frame to the canvas
+  console.log("photo taken");
+  ctx.drawImage(player, 0, 0, canvas.width, canvas.height);
   
-  // Capture a photo by fetching the current contents of the video
-  // and drawing it into a canvas, then converting that to a PNG
-  // format data URL. By drawing it on an offscreen canvas and then
-  // drawing that to the screen, we can change its size and/or apply
-  // other changes before drawing it.
 
-  function takepicture() {
-    var context = canvas.getContext('2d');
-    if (width && height) {
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(video, 0, 0, width, height);
-    
-      var data = canvas.toDataURL('image/png');
-      photo.setAttribute('src', data);
-    } else {
-      clearphoto();
-    }
-  }
+  document.body.appendChild(newCanvas);
+}
 
-  // Set up our event listener to run the startup process
-  // once loading is complete.
-  window.addEventListener('load', startup, false);
-})();
-
-
-
-
-
-
+// 1 second timer to take pics
+timer = () => {
+  setInterval(() => picture(), 1000);   
+}
 
 
